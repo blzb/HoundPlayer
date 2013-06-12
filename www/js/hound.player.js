73c5a3b39c3457e3a7153d5c7a3e6ded6d2aca1c
Angel pimentel <angelpmza@gmail.com>
2013-06-11 22:34:07 -0500

    ui change

--- www/js/hound.player.js
+++ www/js/hound.player.js
@@ -1,10 +1,14 @@
 hound.player={
     loadApp:function(){
+        var servidor = "http://server-hound.rhcloud.com";
+        if(servidor.indexOf("http://")==-1){
+            servidor = "http://"+servidor;
+        }
         $.mobile.showPageLoadingMsg("a", "Descargando Actualizaciones",false);
-        $.ajax($("#servidor").val()+"/HoundConnector/rs/"+$("#appName").val()+"/versiones")
+        $.ajax(servidor+"/HoundConnector/rs/"+$("#appName").val()+"/versiones")
         .done(function() {
             localStorage.setItem("appName",$("#appName").val());
-            localStorage.setItem("server",$("#servidor").val());
+            localStorage.setItem("server",servidor);
             $.mobile.hidePageLoadingMsg();
             window.location = "loading.html";            
         })
@@ -17,5 +21,13 @@ hound.player={
         hound.config.appName = localStorage.getItem("appName");
         hound.config.remote_server = localStorage.getItem("server")+"/HoundConnector/rs/";
         hound.config.remote_server_files = localStorage.getItem("server")+"/HoundConnector/resources/";
+    },
+    loadValues: function(){
+        if(localStorage.getItem("appName")){
+            $("#appName").val(localStorage.getItem("appName"));
+        }
+        if(localStorage.getItem("server")){
+            $("#servidor").val(localStorage.getItem("server"));
+        }
     }
 }
